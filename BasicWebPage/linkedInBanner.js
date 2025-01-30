5579871bbc5d706f37e7e02eeeef5057209df40f
[Corinne Padilla] <[corinnepadilla@yahoo.com]>
2025-01-30 06:02:45 -0800

    adding LinkedIn Banner I created for my Profile page

---
+++ BasicWebPage/linkedInBanner.js
@@ -0,0 +1,13 @@
+//This is my js page
+// This is my js page
+
+let firstName = "Corinne"; // Declare and assign value to firstName
+
+// Display the first statement
+document.getElementById("firstStatement").innerText = '1. console.log("My name is " + firstName)';
+
+// Display the second statement (declaration of firstName)
+document.getElementById("secondStatement").innerText = '2. let firstName = "Corinne"';
+
+// Display the output (result of the console log)
+document.getElementById("output").innerText = '3. print: My name is ' + firstName;
