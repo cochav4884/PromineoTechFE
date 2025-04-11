5a963e646e8d3535b6916cf3141afbece44325b9
[Corinne Padilla] <[corinnepadilla@yahoo.com]>
2025-04-11 16:23:59 -0700

    adding changes to toolbarbutton and toolbar

--- NotesFE/React/slides-clone/src/Toolbar.tsx
+++ NotesFE/React/slides-clone/src/Toolbar.tsx
@@ -1,10 +1,12 @@
 import ToolbarButton from "./ToolbarButton";
+import colorIcon from "./assets/palette-solid (1).svg";
+import fontIcon from "./assets/text-height-solid.svg";
 
 export default function Toolbar() {
     return (
         <div className="bg-light p-3 border-bottom ">
-            <ToolbarButton />
-            <ToolbarButton />
+            <ToolbarButton icon={ colorIcon } onClick={() => alert("Set font color!")} />
+            <ToolbarButton icon={ fontIcon } onClick={() => alert("Set font family!")} />
         </div>
     );
 }
--- NotesFE/React/slides-clone/src/ToolbarButton.tsx
+++ NotesFE/React/slides-clone/src/ToolbarButton.tsx
@@ -1,13 +1,18 @@
-export default function ToolbarButton() {
 
-    const handleButtonClick = () => {
-        alert("Button clicked!");
-    };
-    return (
-        <button className="btn btn-outline-secondary me-2"
-        onClick={handleButtonClick}
-        >
-        Button
-        </button>
-    );
+type ToolbarButtonProps = {
+    icon: string,
+    onClick: () => void
+}
+
+export default function ToolbarButton({ icon, onClick}: ToolbarButtonProps) {
+   return (
+    <button 
+        className="btn btn-outline-secondary me-2" 
+        type="button"  
+        onClick={onClick}
+        title="Toolbar button"
+    >        
+        <img src={icon} alt="Toolbar icon" style={{ width: "1rem"}} />
+    </button>
+  );
 }
