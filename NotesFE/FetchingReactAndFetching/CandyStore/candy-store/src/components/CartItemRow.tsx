6147186bfb3d66747b737013a742a646aea9df4b
[Corinne Padilla] <[corinnepadilla@yahoo.com]>
2025-05-03 07:54:02 -0700

    all is working except that my cart remains empty

--- NotesFE/FetchingReactAndFetching/CandyStore/candy-store/src/components/CartItemRow.tsx
+++ NotesFE/FetchingReactAndFetching/CandyStore/candy-store/src/components/CartItemRow.tsx
@@ -2,22 +2,25 @@ import type { CartItem, Product } from "../types";
 
 type Props = {
   item: CartItem;
-  products: Product[]
+  products: Product[];
 };
+
 export default function CartItemRow({ item, products }: Props) {
-    const product = products.find(p => p.id === item.productId)
+  const product = products.find((p) => p.id === item.productId);
+
+  if (!product) {
+    return (
+      <tr>
+        <td colSpan={3}>PRODUCT NOT FOUND</td>
+      </tr>
+    );
+  }
+
   return (
-    <>
-      <tr>
-        <th>Product</th>
-        <th>Price</th>
-        <th>Quantity</th>
-      </tr>
-      <tr>
-        <td>{product?.name || "PRODUCT NOT FOUND"}</td>
-        <td>${product?.price.toFixed(2)}</td>
-        <td>{item.amount}</td>
-      </tr>
-    </>
+    <tr>
+      <td>{product.name}</td>
+      <td>${product.price.toFixed(2)}</td>
+      <td>{item.amount}</td>
+    </tr>
   );
 }
